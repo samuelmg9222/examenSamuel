aa89435f7d28c1432916928de655c74c31519354
Samuel <samuelmg92@educastur.es>
2024-03-18 12:29:17 +0100

    Añadidos los primeros ficheros al proyecto

---
+++ examenSamuel/src/main/java/miexamen/ClaseExamen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package miexamen;
+
+/**
+ *
+ * @author alu16d
+ */
+public class ClaseExamen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
