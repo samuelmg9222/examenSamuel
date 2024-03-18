10ca8fd9b6b463eb29b7641e63dc72028b5daf12
Samuel <samuelmg92@educastur.es>
2024-03-18 12:48:08 +0100

    Añadir comentarios documentacion

--- examenSamuel/src/main/java/miexamen/ClaseExamen.java
+++ examenSamuel/src/main/java/miexamen/ClaseExamen.java
@@ -1,16 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package miexamen;
-
 /**
- *
- * @author alu16d
+ * 
+ * @author Samuel Muniz Gonzalez
+ * <h1>Entornos de desarrolo ut3</h1>
+ * <p>Esta clase sirve para realizar el examen de la ud3 del modulo de entornos de desarrolo</p>
+ * <a href="https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU">Curso git y github de pildoras informaticas"</a>
+ * @version 1.0.0
+ * @since 18/03/2024
  */
+
+
+
+
 public class ClaseExamen {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Estoy haciendo el examen de la ut3");
+ 
     }
+    
 }
